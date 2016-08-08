8f2542376a5ed3617cbdf201948a59d45b9f718f
Steven Yearian <syearian@gmail.com>
2016-08-08 13:51:37 -0500

    Add files from project-template

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var dev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+	entry: './dist/app.js',
+	output: {
+		path: dev ? "builds/development" : 'builds/production',
+		filename: 'app.js'
+	},
+	devServer: {
+		inline: true,
+		contentBase: './build',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!sass-loader'
+			}, 
+				{
+			    test: /\.(png|jpg)$/,
+			    loader: 'url-loader?limit=10000'
+			}
+	 ]
+	}
+};
+
+
+
+
+
