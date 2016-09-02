6dc9d441f01c8fc0b33765631c7ab7d456444e20
Steven Yearian <syearian@gmail.com>
2016-09-02 12:54:11 -0500

    Change markdownText->inpuText in handleChangeText
    Also edit initialText

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -15,7 +15,7 @@ marked.setOptions({
   smartypants: true
 });
 var initialText = {
-  text: "# h1 Heading\n## h2 Heading\n### h3 Heading\n#### h4 Heading\n##### h5 Heading\n###### h6 Heading\n\n## Horizontal Rules\n\n___\n\n***\n\n***\n\nMarkdown -- awesome\n\n\"Smartypants, double quotes\"\n\n'Smartypants, single quotes'\n\n## Emphasis\n\n**This is bold text**\n\n__This is bold text__\n\n*This is italic text*\n\n_This is italic text_\n\n~~Deleted text~~\n\n## Blockquotes\n\n> Blockquotes can also be nested...\n>> ...by using additional greater-than signs right next to each other...\n> > > ...or with spaces between arrows.\n\n## Lists\n\nUnordered\n\n+ Create a list by starting a line with `+`, `-`, or `*`\n+ Sub-lists are made by indenting 2 spaces:\n  - Marker character change forces new list start:\n    * Ac tristique libero volutpat at\n    + Facilisis in pretium nisl aliquet\n    - Nulla volutpat aliquam velit\n+ Very easy!\n\nOrdered\n\n\n1. Lorem ipsum dolor sit amet\n2. Consectetur adipiscing elit\n3. Integer molestie lorem at massa\n\n## Links\n\n[link text](http://dev.nodeca.com)\n\n[link with title](http://nodeca.github.io/pica/demo/ 'title text!')\n\nAutoconverted link https://github.com/nodeca/pica"
+  text: "# h1 Heading\n## h2 Heading\n### h3 Heading\n#### h4 Heading\n##### h5 Heading\n###### h6 Heading\n\n## Horizontal Rules\n\n___\n\n***\n\n***\n\nMarkdown -- awesome\n\n\"Smartypants, double quotes\"\n\n'Smartypants, single quotes'\n\n## Emphasis\n\n**This is bold text**\n\n__This is bold text__\n\n*This is italic text*\n\n_This is italic text_\n\n~~Deleted text~~\n\n## Blockquotes\n\n> Blockquotes can also be nested...\n>> ...by using additional greater-than signs right next to each other...\n> > > ...or with spaces between arrows.\n\n## Lists\n\nUnordered\n\n+ Create a list by starting a line with `+`, `-`, or `*`\n+ Sub-lists are made by indenting 2 spaces:\n  - Marker character change forces new list start:\n    * Ac tristique libero volutpat at\n    + Facilisis in pretium nisl aliquet\n    - Nulla volutpat aliquam velit\n+ Very easy!\n\nOrdered\n\n1. Lorem ipsum dolor sit amet\n2. Consectetur adipiscing elit\n3. Integer molestie lorem at massa\n\n## Links\n\n[link text](http://dev.nodeca.com)\n\n[link with title text](http://nodeca.github.io/pica/demo/ 'title text!')\n\nAutoconverted link https://github.com/nodeca/pica"
 }
 
 var App = React.createClass( {
@@ -27,7 +27,7 @@ var App = React.createClass( {
   },
   handleChangeText: function(event) {
     this.setState({
-        markdownText: event.target.value,
+        inputText: event.target.value,
         outputText: { __html: marked(event.target.value)}
       }
     );
